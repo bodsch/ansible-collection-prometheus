abc92f9327484b97e8a5d42178d613fd7799cc09
Bodo Schulz <bodo@boone-schulz.de>
2026-02-26 09:09:48 +0100

    add logstream_exporter

---
+++ roles/logstream_exporter/molecule/check_mode/tests/test_default.py
@@ -0,0 +1,35 @@
+from __future__ import annotations, unicode_literals
+
+import os
+
+import pytest
+import testinfra.utils.ansible_runner
+from helper.molecule import get_vars, infra_hosts, local_facts
+
+testinfra_hosts = infra_hosts(host_name="instance")
+
+# --- tests -----------------------------------------------------------------
+
+
+@pytest.mark.parametrize(
+    "files",
+    [
+        "/usr/bin/logstream_exporter",
+        "/etc/logstream_exporter/config.yml",
+        "/lib/systemd/system/logstream_exporter.service",
+    ],
+)
+def test_files(host, files):
+    f = host.file(files)
+    assert not f.exists
+
+
+@pytest.mark.parametrize(
+    "sockets",
+    [
+        "tcp://127.0.0.1:7979",
+    ],
+)
+def test_socket(host, sockets):
+    s = host.socket(sockets)
+    assert not s.is_listening
---
+++ roles/logstream_exporter/molecule/latest/tests/test_default.py
@@ -0,0 +1,95 @@
+from __future__ import annotations, unicode_literals
+
+import os
+
+import pytest
+import testinfra.utils.ansible_runner
+from helper.molecule import get_vars, infra_hosts, local_facts
+
+testinfra_hosts = infra_hosts(host_name="instance")
+
+# --- tests -----------------------------------------------------------------
+
+
+@pytest.mark.parametrize(
+    "files",
+    [
+        "/usr/bin/logstream-exporter",
+        "/etc/logstream_exporter/config.yml",
+        "/lib/systemd/system/logstream_exporter.service",
+    ],
+)
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+
+
+def test_version(host, get_vars):
+    """ """
+    distribution = host.system_info.distribution
+    release = host.system_info.release
+
+    print(f"distribution: {distribution}")
+    print(f"release     : {release}")
+
+    version = local_facts(host=host, fact="logstream_exporter").get("version")
+
+    install_dir = get_vars.get("logstream_exporter_install_path")
+    defaults_dir = get_vars.get("logstream_exporter_defaults_directory")
+    config_dir = get_vars.get("logstream_exporter_config_dir")
+
+    if "latest" in install_dir:
+        install_dir = install_dir.replace("latest", version)
+
+    files = []
+    files.append("/usr/bin/logstream_exporter")
+
+    if install_dir:
+        files.append(f"{install_dir}/logstream_exporter")
+    if defaults_dir and not distribution == "artix":
+        files.append(f"{defaults_dir}/logstream_exporter")
+    if config_dir:
+        files.append(f"{config_dir}/config.yml")
+
+    print(files)
+
+    for _file in files:
+        f = host.file(_file)
+        assert f.is_file
+
+
+def test_user(host, get_vars):
+    """ """
+    user = get_vars.get("logstream_exporter_system_user", "logstream_exporter")
+    group = get_vars.get("logstream_exporter_system_group", "logstream_exporter")
+
+    assert host.group(group).exists
+    assert host.user(user).exists
+    assert group in host.user(user).groups
+    assert host.user(user).home == "/nonexistent"
+
+
+def test_service(host, get_vars):
+    service = host.service("logstream_exporter")
+    assert service.is_enabled
+    assert service.is_running
+
+
+def test_open_port(host, get_vars):
+    for i in host.socket.get_listening_sockets():
+        print(i)
+
+    logstream_exporter_server = get_vars.get("logstream_exporter_server", {})
+
+    print(logstream_exporter_server)
+
+    if isinstance(logstream_exporter_server, dict):
+        listen_address = logstream_exporter_server.get("metrics_listen_address")
+
+    if not listen_address:
+        listen_address = "127.0.0.1:9212"
+
+    print(listen_address)
+
+    service = host.socket(f"tcp://{listen_address}")
+    assert service.is_listening
